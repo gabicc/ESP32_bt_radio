67a3e85647509980e7679a08162bd647dd541763
Gabriel Cocan <gabriel.calin.cocan@gmail.com>
2026-04-20 23:08:10 +0300

    First variant

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ESP32_bt_radio
 Bluetooth radio via ESP 32
+
+
+## ESP32-S Wiring with CP2102(USB-to-UART)
+
+
+
+| CP2102 Pin | Connect To ESP32 Pin | Power source | 	Purpose |
+|------|------|---------|------|
+| TXD	| RX0 (GPIO3) | |	CP2102 transmits → ESP32 receives |
+|RXD |	TX0 (GPIO1) | |	CP2102 receives ← ESP32 transmits|
+|GND 	|GND | |	Common ground|
+| | GND | - connector | |
+|  |	3.3V | + connector |	|
+|  | GPIO 0 | - connector | Use this for uploading source code |
+
+
+## ESP32-S Wiring with MAX98357A (I2S Sound amplifier)
+
+| MAX98357A Pin | ESP32 Pin | 	Purpose |
+|------|------|------|
+| LRC	| D14 |	 |
+|BCLK |	D27 | |
+| DIN	| D26 |	|
+| GND | GND | |
+| VIN | VIN | |
+
+
+Sources: https://www.otronic.nl/nl/aansluitschemas-met-voorbeeldcode-voor-arduino/max98357a-wifi-radio-aansluitschema-en-voorbeeldco/
+https://www.instructables.com/DIY-Bluetooth-Speaker-Using-Esp32-With-Controls/
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,33 @@
 #include <Arduino.h>
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
+
+#define I2S_LRC 14
+#define I2S_BCLK 27
+#define I2S_DIN 26
 
 // put function declarations here:
-int myFunction(int, int);
+
+I2SStream i2s;
+BluetoothA2DPSink a2dp_sink(i2s);
 
 void setup() {
   Serial.begin(115200);
 
-  Serial.println("Hello, Application started");
+  Serial.println("Hello, Application STARTED");
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  auto cfg = i2s.defaultConfig();
+  cfg.pin_bck = I2S_BCLK;  // BCLK pin
+  cfg.pin_ws = I2S_LRC;   // LRC pin
+  cfg.pin_data = I2S_DIN; // DIN pin
+  i2s.begin(cfg);
+
+  a2dp_sink.start("ESPEAKER");
+  a2dp_sink.set_volume(64);
 }
 
 void loop() {
-  Serial.println("Loop started");
-  delay(1000);
+  //Serial.println("Loop started");
+  //delay(1000);
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
